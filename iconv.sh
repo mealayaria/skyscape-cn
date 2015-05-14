4a295cb9699d54c9b3383182a78dd0c1b8a8bb3a
chiaya <hnxmswyy@gmail.com>
2015-05-14 09:14:41 +0800

    script and s01 updated

--- iconv.sh
+++ iconv.sh
@@ -3,8 +3,13 @@
 cd $1
 for filename in `ls *.ks`
 do
-    iconv -f CP936 -t UTF-8 $filename > $filename".tmp"
-    mv $filename".tmp" $filename
+    if iconv -f CP936 -t UTF-8 $filename > $filename".tmp"
+    then 
+        mv $filename".tmp" $filename
+    else
+        rm $filename".tmp"
+        continue
+    fi
 done
 
 #ls | while read lines
